f93915eecacdd4e1c5915ca7ab5df53bd109a5fa
TanabutT <t_tanabut@hotmail.com>
2022-12-19 00:56:01 +0700

    backward to AirflowIGNOREHEADER 1

--- dags/sql_insert_parquet.py
+++ dags/sql_insert_parquet.py
@@ -5,25 +5,29 @@ load_owners = """COPY %s FROM 's3://petclinic13/previous_parquet/owners.parquet'
 	ACCESS_KEY_ID '%s'
 	SECRET_ACCESS_KEY '%s'
 	SESSION_TOKEN '%s'
-	FORMAT AS parquet""" % ( 'owners', aws_access_key_id , aws_secret_access_key , aws_session_token)
+	FORMAT AS parquet
+	IGNOREHEADER 1""" % ( 'owners', aws_access_key_id , aws_secret_access_key , aws_session_token)
 
 load_phistory = """COPY %s FROM 's3://petclinic13/previous_parquet/phistory.parquet'
 	ACCESS_KEY_ID '%s'
 	SECRET_ACCESS_KEY '%s'
 	SESSION_TOKEN '%s'
-	FORMAT AS parquet""" % ( 'phistory', aws_access_key_id , aws_secret_access_key , aws_session_token)
+	FORMAT AS parquet
+	IGNOREHEADER 1""" % ( 'phistory', aws_access_key_id , aws_secret_access_key , aws_session_token)
 
 load_pdetail = """COPY %s FROM 's3://petclinic13/previous_parquet/pdetail.parquet'
 	ACCESS_KEY_ID '%s'
 	SECRET_ACCESS_KEY '%s'
 	SESSION_TOKEN '%s'
-	FORMAT AS parquet""" % ( 'pdetail', aws_access_key_id , aws_secret_access_key , aws_session_token)
+	FORMAT AS parquet
+	IGNOREHEADER 1""" % ( 'pdetail', aws_access_key_id , aws_secret_access_key , aws_session_token)
 
 load_pets = """COPY %s FROM 's3://petclinic13/previous_parquet/pets.parquet'
 	ACCESS_KEY_ID '%s'
 	SECRET_ACCESS_KEY '%s'
 	SESSION_TOKEN '%s'
-	FORMAT AS parquet""" % ( 'pets', aws_access_key_id , aws_secret_access_key , aws_session_token)
+	FORMAT AS parquet
+	IGNOREHEADER 1""" % ( 'pets', aws_access_key_id , aws_secret_access_key , aws_session_token)
 
 loadinto_table_queries = [     
     load_owners
