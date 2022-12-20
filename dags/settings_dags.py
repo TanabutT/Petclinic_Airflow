4c2df11ef4a3054f649f8565d7b6926d98ea301b
TanabutT <t_tanabut@hotmail.com>
2022-12-20 02:39:38 +0000

    setting

--- dags/settings_dags.py
+++ dags/settings_dags.py
@@ -1,8 +1,6 @@
-aws_access_key_id = "ASIAWFHJWX4CRZAGJFEG"
-aws_secret_access_key = "EOkLKJ+j9GtSC7SDQHjjTcMdq/NArY5cN6zSpXSo"
-aws_session_token = "FwoGZXIvYXdzECEaDPcoawywmqrTsZaqgSLHAauh5axKUGgkGKPbml3Lx//PNzBIks6JPNtWct3AYNCCZo+b3eMErBNsHFQnzeapv0e0XySXDDbUzMyn+iZRc7WfNMSgk0GpPD5VReLAVBKjCB+3Chgu5zHIPtxeKvRBQ/Nyh6798bmfHDSZY1D6YXqX+W/DoBC1R/yoTMQuLK63OSKFaoMrVgGysgrZOerZ/duM9kAyR437PtR0RjQzpt/d/9kDINyCqFNgtxL4VCnEqMyWndj9jlHAt/VFQQ19K9UulXgJJOIoveH8nAYyLUK7YoXGxHYFePApL3/QcppjCe/u0ad9LBCvmTm7YRQtJdi3/hIhcHjpIld2lg=="
-
-
+aws_access_key_id = "ASIAWFHJWX4CSLZGJS27"
+aws_secret_access_key = "T2t9AcnBwIVAbYJUTrO2Y1zj6/ZyVmcczCSNFKQt"
+aws_session_token = "FwoGZXIvYXdzEEMaDCiq4vt/wyMAy9O7AyLHAViW/BPqjSTS+f/NKSh4SH2uuwcTQ/tsAf+PuQWnFokHpTIdZBotSo3p0HGJQ1lQuFyXNdkevA5PSV+HHOmYTPjWdtd3gxA/OtkfpWSYMp4BjGOhUjWB2PjGYWjwu2nm+A1YeRI5zXmLbWPNqEUjAfKS8IKJbdZTyRoXfgbTdLHTMBK4XErM8o2v0Qwe7s2BgHAuSeecyNdMNwEFzgUB5twUchP4XclD6H9xLZbiMYXqhc8b0UJ/wMfNtWYAo9rtRWUh/y2rOc8o7peEnQYyLd6UmwccdFEOayJ5sxhm1WMa3N5Ec2liPYiZIkMOXPNiiHuXjzLe/3yiB0zgFA=="
 
 
 
@@ -11,3 +9,4 @@ aws_session_token = "FwoGZXIvYXdzECEaDPcoawywmqrTsZaqgSLHAauh5axKUGgkGKPbml3Lx//
 #Redshift Cluster "can not create" LabRole unable to access Redshift via boto3
 cluster_user = "awsuser"
 cluster_password = "eazhiKb228WYGZs"
+
